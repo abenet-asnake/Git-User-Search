2b6ee7d87183054a14aad59d3718d00e6629bfad
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-07-25 11:17:14 +0300

    server js file serve the stic file from public folder that run the public folder

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ const express=require('express');
 const app = express();
 const axios = require('axios');
 const port=3000;
+
+// serve static files from public folder
+app.use(express.static('public'));
