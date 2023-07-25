f44aa7e66eef587146a1d13c3f8e20024b80984d
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-07-25 11:20:37 +0300

    server creating

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const port=3000;
 
 // serve static files from public folder
 app.use(express.static('public'));
+
+app.listen(port, () => {  
+    console.log(`The server is running on http://localhost:${port}`);   
+});
