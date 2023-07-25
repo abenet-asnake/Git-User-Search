db152a0d5b78e64be92c544940b8905ad4ba0b8c
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-07-25 11:49:31 +0300

    declaring user name and axios to get api of git hub

---
+++ public/script.js
@@ -0,0 +1,8 @@
+function searchUser(){
+    const username= document.getElementById('gitusername').value;
+    if(!username){
+        alert('Please enter a GitHub username');
+        return;
+    }
+    axios.get(`https://api.github.com/users/${username}`);
+}
